7c55003cc27b7290886781a6d683abba9c7ce173
edrianprones12 <edrian.prones12@gmail.com>
2026-09-18 15:24:06 +0800

    solution Group 2

---
+++ group2.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  print('Enter a Positive Integer:');
+  String? input = stdin.readLineSync();
+
+  int?number = int.tryParse(input ?? '');
+
+  if (number != null && number > 0) {
+    print('You entered: $number');
+
+    print('Factorial of $number is: ${factorial(number)}');
+  }else {
+    print('Invalid input. Please enter a positive integer.');
+  }
+  
+}
+
+int factorial(int n) {
+  if (n<=1) {
+    return 1;
+  } 
+  return n * factorial(n - 1);
+  
+}
